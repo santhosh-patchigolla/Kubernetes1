bfaea56757e0b7911b059aa72825844be57d4f4e
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-08-31 13:42:10 +0530

    sdc

--- eks/data.tf
+++ eks/data.tf
@@ -3,7 +3,7 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket = "b54-tf-remote-state"
+    bucket = "b54-santhosh-terraform-remote-state"
     key    = "vpc/${var.ENV}/terraform.tfstate"
     region = "us-east-1"
   }
--- eks/env-dev/dev-backend.tfvars
+++ eks/env-dev/dev-backend.tfvars
@@ -1,4 +1,4 @@
-bucket = "b54-tf-remote-state"
+bucket = "b54-santhosh-terraform-remote-state"
 key    = "eks/dev/terraform.tfstate"
 region = "us-east-1"
 
